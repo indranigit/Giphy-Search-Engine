b63ef02f9756565601aac344a6ffebf1ee039ec3
Indrani Sen <sen.indrani0608@gmail.com>
2021-09-05 15:05:28 +0530

    updated main.js

--- javascript/main.js
+++ javascript/main.js
@@ -4,13 +4,10 @@ var button = document.querySelector("button");
 button.addEventListener("click", function() {
 var input = document.querySelector("input").value;
 getInput(input);
-  
-  
 });
 
 var x = document.querySelector(".js-userinput");
 x.addEventListener("keyup", function(e) {
-
 var input = document.querySelector("input").value;
 
 // 13 is key code for Enter
@@ -19,32 +16,23 @@ getInput(input);
 }
 });
 
-
-
 // Interacting with the Giphy API
 
 function getInput(item) {
   var query = item.split(' ').join('+')
-  
-
   var url = "https://api.giphy.com/v1/gifs/search?q="+ query + "&api_key=SDEsWMHoj4DO7LFMxWFHlVJVkElcDm8h";
 
-
-
-
 // AJAX Request
+
 var GiphyAJAXCall = new XMLHttpRequest();
 GiphyAJAXCall.open( 'GET', url );
 GiphyAJAXCall.send();
-
 
 GiphyAJAXCall.addEventListener('load', function(e) {
 var data = e.target.response
 pushToDom(data)
 });
-
 }
-
 
 // Showing the gifs
 
@@ -52,10 +40,9 @@ function pushToDom(input) {
   var response = JSON.parse(input);
   var f = document.querySelector(".js-container");
   var result = document.querySelector(".results");
-  
+
   clear(f);
   clear(result);
-    
       var imageUrls = response.data
 
       imageUrls.forEach(function(gif) {
@@ -63,14 +50,9 @@ function pushToDom(input) {
 
         result.innerHTML = src.length + " gifs found";
         f.innerHTML += "<img src=\"" + src + "\" class=\"container-image\">";
-    
-
       })
-
-     
+      
       function clear(item) {
         item.innerHTML = "";
     }
-
-
 }
